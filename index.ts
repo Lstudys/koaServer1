8628997e424de7a8688d203b38b791d6ce6b9e8f
Lstudys <Lstudys@163.com>
2022-05-12 21:50:53 +0800

    引入路由，有问题，提交标记一下

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-// const Koa = require('koa');
+/**
+ * 入口文件
+ */
 import koa from 'koa';
+
+import router from './src/routes/index.js'
 
 const app = new koa();
 
-app.use(async (ctx) =>{
-    ctx.body = 'hello index';
-})
+app.use(router);
+
+// app.use(async (ctx) =>{
+//     ctx.body = 'hello index';
+// })
 app.listen(3000);
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import Router from 'koa-router';
+
+const router = new Router();
+
+router.get('/', async (ctx) =>{
+    ctx.body = 'router';
+    // next();
+});
+
+export default router;
